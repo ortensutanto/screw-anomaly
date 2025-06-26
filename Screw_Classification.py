e4fd6155a9ef1ad32566cebf00ba60a9b9522526
Orten <ortensutanto@gmail.com>
2025-06-26 15:34:00 +0700

    Removed pyscrew import and replaced it with pandas read_csv

--- Screw_Classification.py
+++ Screw_Classification.py
@@ -8,12 +8,10 @@ from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import sklearn
-import pyscrew
 from sktime.utils import mlflow_sktime
 
-data = get_data(scenario="s03")
 
-df = pd.DataFrame(data)
+df = pd.read_csv("s03.csv")
 
 @st.cache_resource
 def load_models():
